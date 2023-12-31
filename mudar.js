01cf8809d272fe5baddedc525e739a60f5818d33
Leonardo Alaff <leonardoalaff@gmail.com>
2023-12-31 18:55:06 -0300

    1.3

--- mudar.js
+++ mudar.js
@@ -1,10 +1,36 @@
+// **** VARIÁVEIS GERAIS ****
+const sessao1 = document.querySelector('.sessao1')
+const verduras = document.querySelector('.verduras')
+
+// **** ALFACE ****
 const alface2 = document.querySelector('.alface2')
 const fundo = document.querySelector('.fundo')
 const check = document.querySelector('#check')
-const fundoTexto = document.querySelector('.fundo-texto')
+const textoAlface = document.querySelector('.texto-alface')
+const tituloAlface = document.querySelector('.titulo-alface')
 
 check.addEventListener('change', () => {
     alface2.classList.toggle('active')
     fundo.classList.toggle('active')
-    document.body.classList.toggle('active')
+    sessao1.classList.toggle('alface')
+    verduras.classList.toggle('active')
+    tituloAlface.classList.toggle('active')
+    textoAlface.classList.toggle('active')
 })
+
+// **** TOMATE ****
+const checkTomate = document.querySelector('#check-tomate')
+const tomate2 = document.querySelector('.tomate2')
+const tomate = document.querySelector('.tomate')
+const textoTomate = document.querySelector('.texto-tomate')
+const tituloTomate = document.querySelector('.titulo-tomate')
+
+checkTomate.addEventListener('change', () => {
+    tomate2.classList.toggle('active')
+    tomate.classList.toggle('active')
+    verduras.classList.toggle('active')
+    sessao1.classList.toggle('active-tomate')
+    textoTomate.classList.toggle('active')
+    tituloTomate.classList.toggle('active')
+})
+
